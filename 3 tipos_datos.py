03a90f194fd73ad05bb9c1f0e140533c44294d92
MonkeyPyLuffy <hbrd1204@gmail.com>
2024-08-09 20:04:31 -0600

    Create 3 tipos_datos.py
    init capitulo 3

---
+++ 3 tipos_datos.py
@@ -0,0 +1,11 @@
+#TIPOS DE DATOS
+
+#"""Cada «trozo» de memoria contiene realmente un objeto, de ahí que se diga que en Python
+#todo son objetos. Y cada objeto tiene, al menos, los siguientes campos:
+#• Un tipo del dato almacenado.
+#• Un identificador único para distinguirlo de otros objetos.
+#• Un valor consistente con su tipo.
+
+#ejemplo de booleano
+
+aprobado = True
